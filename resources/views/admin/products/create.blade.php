7a7bb3d996b876041c600c69bce63da8c8dd39d0
Nanderson Castro <nandokstro@gmail.com>
2020-06-18 20:08:28 -0300

    Fix input file mensagens de validacao do input file

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -63,7 +63,7 @@
         <div class="form-group">
             <label>Fotos do Produto</label>
             <input type="file" name="photos[]" class="form-control @error('photos.*') is-invalid @enderror" multiple>
-            @error('photos')
+            @error('photos.*')
             <div class="invalid-feedback">
                 {{$message}}
             </div>
